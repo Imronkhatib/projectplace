2a3dbada75b3abf460b9a905745d997f035cc29c
Imron Khatib <imronskhatib@gmail.com>
2018-04-04 20:26:25 -0500

    create joint project_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405011701) do
+ActiveRecord::Schema.define(version: 20180405012425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "project_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "project_name"
