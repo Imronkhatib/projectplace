7762d4619e23e32dc1c306cbb2047f23dde0a7cf
Imron Khatib <imronskhatib@gmail.com>
2018-04-18 18:45:57 -0500

    Create front-end user Authentication & route for profile page

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 /* global Vue, VueRouter, axios */
+// homepage is the sign up page
 
-var HomePage = {
-  template: "#home-page",
+
+var ProfilePage = {
+  template: "#profile-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome User!"
     };
   },
   created: function() {},
@@ -12,8 +14,79 @@ var HomePage = {
   computed: {}
 };
 
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/profile", component: ProfilePage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
